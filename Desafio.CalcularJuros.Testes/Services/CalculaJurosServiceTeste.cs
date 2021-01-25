54f5c90dd88680827432d10bc4a4992388540363
Evandro Barbosa dos Reis <evandrobarbosadosreis@gmail.com>
2021-01-25 18:17:37 -0300

    implementada a segunda api

--- Desafio.CalcularJuros.Testes/Services/CalculaJurosServiceTeste.cs
+++ Desafio.CalcularJuros.Testes/Services/CalculaJurosServiceTeste.cs
@@ -3,19 +3,19 @@ using Xunit;
 
 namespace Desafio.CalcularJuros.Testes.Services
 {
-    public class CalculadoraJurosServiceTeste
+    public class CalculaJurosServiceTeste
     {
         [Fact]
         public void DeveCalcularJurosCorretamente()
         {
             //Given
-            var valorAplicado = 100;
+            var valorAplicado = 100m;
             var taxaJuros = 0.01;
             var periodoEmMeses = 5;
-            var valorFinalEsperado = 105.10;
+            var valorFinalEsperado = 105.10m;
             
             //When
-            var valorFinal = CalculadoraJurosService.CalcularJuros(
+            var valorFinal = CalculaJurosService.CalcularJuros(
                 valorAplicado, 
                 taxaJuros, 
                 periodoEmMeses);
